9d266c8dd98fa0b5c3c97aeac0fe862888db58f2
Abdallah <ahkortam@gmail.com>
2022-01-23 03:54:11 +0200

    first commit

---
+++ Wifi password.py
@@ -0,0 +1,23 @@
+import subprocess
+
+data = subprocess.check_output(
+    ['netsh', 'wlan', 'show', 'profiles']).decode('utf-8').split('\n')
+
+# Enter the network name
+profile = input("Enter networks name: ")
+
+
+try:
+    password = subprocess.check_output(
+        ['netsh', 'wlan', 'show', 'profile', profile, 'key=clear']).decode('utf-8').split('\n')
+        
+    password = [passwd.split(':')[1][1:-1]
+                for passwd in password if "Key Content" in passwd]
+except subprocess.CalledProcessError:
+    print("Error: plz enter the a correct name.")
+
+
+try:
+    print("{:<30}|  {:<}".format(profile, password[0]))
+except NameError:
+    print("")
